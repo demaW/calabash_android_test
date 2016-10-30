bb1f83c740115affa44b55b7a4abf53c53e7a642
demaW <demavov@gmail.com>
2016-10-30 16:25:24 +0200

    clean ups

--- vendor/bundle/ruby/2.3.0/bin/htmldiff.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"E:\ProgramFiles\Ruby23-x64\bin\ruby.exe" "D:/dev/ruby/helloworld/vendor/bundle/ruby/2.3.0/bin/htmldiff" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"E:\ProgramFiles\Ruby23-x64\bin\ruby.exe" "%~dpn0" %*
--- vendor/bundle/ruby/2.3.0/gems/retriable-2.0.2/lib/retriable/core_ext/kernel.rb
+++
@@ -1,7 +0,0 @@
-require "retriable"
-
-module Kernel
-  def retriable(opts = {}, &block)
-    Retriable.retriable(opts, &block)
-  end
-end
